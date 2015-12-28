40e2a9b4fb0a6a36e3377ae8ae04717e626c42fb
Yongtao Hu <ythu@cs.hku.hk>
2015-12-28 21:11:35 +0800

    refine search: no list bullets

--- assets/js/search.js
+++ assets/js/search.js
@@ -43,7 +43,7 @@ jQuery(function() {
           var item = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
-          var appendString = '<a href="' + item.url + '">' + item.title + '</a>';
+          var appendString = '<a href="' + item.url + '">' + item.title + '</a><br />';
 
           // Add it to the results
           $search_results.append(appendString);
